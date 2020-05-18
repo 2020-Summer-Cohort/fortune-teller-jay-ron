7a5c778ac909a17f08fbbb494a735dcba8f1c682
roncwynn <rcwynn@gmail.com>
2020-05-18 14:50:04 -0400

    Part 1 version 1 complete.

--- FortuneTeller/Program.cs
+++ FortuneTeller/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
 
 namespace FortuneTeller
 {
@@ -7,6 +8,36 @@ namespace FortuneTeller
         static void Main(string[] args)
         {
             Console.WriteLine("Hello! Welcome to Fortune Teller");
+
+
+        }
+
+        static void StartGame()
+        {
+            User someUser = new User();
+            GetUserInfo(someUser);
+        }
+        static void GetUserInfo(User someUser)
+        {
+            Console.WriteLine("What is your first name?");
+            someUser.FirstName  = Console.ReadLine();
+
+            Console.WriteLine("What is your last name?");
+            someUser.LastName = Console.ReadLine();
+
+            Console.WriteLine("How old are you?");
+            someUser.Age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the integer to represent the month you were born:");
+            someUser.BirthMonth = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("What is your favorite ROYGBIV color?  Enter 'Help' if you don't know what ROYGBIV is.");
+            someUser.FavoriteColor = Console.ReadLine();
+            //loop
+
+            Console.WriteLine("How many siblings do you have?");
+            someUser.NumberOfSiblings = Convert.ToInt32(Console.ReadLine());
+
         }
     }
 }
---
+++ FortuneTeller/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace FortuneTeller
+{
+    class User
+    {
+        public  string FirstName { get; set; } 
+        public  string LastName { get; set; }
+        public  int Age { get; set; }
+        public  int BirthMonth { get; set; }
+        public  string FavoriteColor { get; set; }
+        public  int NumberOfSiblings { get; set; }
+
+        static User()
+        {
+        }
+
+    }
+}
